6096c439f6ae756193de77454a3940477a5e0424
Arnaud Pion <apion@paloaltonetworks.com>
2024-05-16 15:20:12 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # prisma-cloud-agentless-scanning
 Scripts to update agentless scanning configuration in bulk in Prisma Cloud
+
+## agentless-scanning-bulk-update.py
+This python script allows you to bulk update the agentless scanning configuration of AWS accounts.
+
+### Usage
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 agentless-scanning-bulk-update.py
+
+### Requirements
+- Onboard your AWS accounts in Prisma Cloud. Make sure they have the _Agentless Workload Scanning_ capability selected during onboarding
+- Create an Access Key on Prisma Cloud, you can refer to the [online documentation](https://docs.prismacloud.io/en/enterprise-edition/content-collections/administration/create-access-keys)
+- Create Environment Variables on your system :
+  - **COMPUTE_API_ENDPOINT** : url of your [Prisma Cloud Compute Console](https://pan.dev/prisma-cloud/api/cwpp/access-api-saas/#:~:text=Retrieve%20your%20Compute%20Console's%20address,your%20Prisma%20Cloud%20user%20credentials)
+  - **PRISMA_USERNAME** : your Prisma Cloud Access Key
+  - **PRISMA_PASSWORD** : your Prisma Cloud Secret Key
+- Modify the values of the following variables in the script :
+  - **AWS_REGIONS** : the list of AWS regions you want to scan
+  - **HUB_ACCOUNT** : the name (**not** the account ID), in Prisma Cloud, of the AWS account you want to use as hub account
+
+### Credits
+Stephen Gordon, who worked on the initial script used as baseline for this one
---
+++ agentless-scanning-bulk-update.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+# Script to configure AWS Regions and Hub Account in bulk for Prisma Cloud Agentless Scanning
+# Usage: ./agentless-scanning-bulk-update.py
+
+import os
+import requests
+from requests.packages import urllib3
+
+# Suppress SSL warnings
+urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
+
+# Script variables that need to be updated
+AWS_REGIONS = [] # List of AWS regions that will be scanned, example : AWS_REGIONS = ["eu-west-1","us-east-2"]
+HUB_ACCOUNT = "" # Name of the AWS account used as Hub to scan other accounts, example : HUB_ACCOUNT = "aws-hub"
+
+# Set Credentials
+console_url = os.getenv("COMPUTE_API_ENDPOINT")
+access_key = os.getenv("PRISMA_USERNAME")
+secret_key = os.getenv("PRISMA_PASSWORD")
+
+payload = {
+    'username':access_key,
+    'password':secret_key
+}
+
+# Generate a Token for API authentication to Prisma Cloud
+token = requests.post(console_url+"/api/v1/authenticate", json=payload, verify=0).json()['token']
+
+# Set Prisma Cloud Headers for Login with token
+pccHeaders = {
+    'Authorization': 'Bearer ' + token,
+    'Accept': 'application/json'
+}
+
+# Init variables
+limit = 50
+offset = 0
+success_counter = 0
+overall_counter = 0
+response = True
+
+# Check that variables have been updated
+if AWS_REGIONS != [] and HUB_ACCOUNT != "":
+    while response:
+        payload = {
+            'limit':limit,
+            'offset':offset,
+            'cloudProviders':"aws"
+        }
+
+        # Query Prisma Cloud for existing accounts with agentless scanning enabled
+        response = requests.get(console_url+"/api/v1/cloud-scan-rules", headers=pccHeaders, params=payload, verify=0).json()
+        offset=offset+limit
+
+        # Check API response and iterate through existing accounts
+        if response:
+            for i in response:
+                # Ignore Hub account
+                if i["agentlessScanSpec"]["hubAccount"] == False:
+                    # Update configuration for the account : AWS Regions, Hub. 'Scanners' also needs to be 0 when scanning with a Hub account
+                    i["agentlessScanSpec"]["regions"] = AWS_REGIONS
+                    i["agentlessScanSpec"]["hubCredentialID"] = HUB_ACCOUNT
+                    i["agentlessScanSpec"]["scanners"] = 0
+
+                    # Initialize data and make the API call to update the account with its new configuration
+                    data=[]
+                    data.append(i)
+                    update = requests.put(console_url+"/api/v1/cloud-scan-rules", headers=pccHeaders, json=data, verify=0)
+
+                    # Check if API call was successful and update counters
+                    if update.status_code == 200:
+                        print(" + Successfully updated account :", i["credential"]["accountName"])
+                        success_counter += 1
+                        overall_counter += 1
+                    else:
+                        print(" - Something went wrong with account :", i["credential"]["accountName"])
+                        overall_counter += 1
+
+    # After iterating through all accounts, return the overall results
+    if overall_counter != 0:
+        if overall_counter == success_counter:
+            print(f"   All accounts (total number : {success_counter}) have been successfully updated !")
+        else:
+            print(f"   {success_counter} accounts have been successfully updated\n   !!! {overall_counter - success_counter} accounts have NOT been updated !!!")
+    else:
+        print("   No accounts found on your Prisma Cloud tenant !")
+
+else:
+    print("!!! Please update values for AWS_REGIONS and HUB_ACCOUNT !!!")
